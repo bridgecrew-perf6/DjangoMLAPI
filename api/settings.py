7ac911e22e1fc3b37e005fca673775c30423748e
ghacupha <mailnjeru@gmail.com>
2020-12-20 17:46:45 +0300

    Amend Procfile configuration

--- api/settings.py
+++ api/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predictor',
-    'gunicorn'
+    'predictor'
 ]
 
 MODELS = os.path.join(BASE_DIR, 'predictor/models')
